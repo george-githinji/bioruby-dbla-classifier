2213652b4f6fc169bddf2b0076e00bccb8225dcd
georgeG <georgkam@gmail.com>
2012-04-23 11:07:46 +0300

    remove space

--- lib/bio-dbla-classifier.rb
+++ lib/bio-dbla-classifier.rb
@@ -1,4 +1,3 @@
 require 'bio'
 require File.join(File.expand_path(File.dirname(__FILE__)), 'bio','sequence/aa/aa')
 require File.join(File.expand_path(File.dirname(__FILE__)), 'bio','sequence/na/na')
-
