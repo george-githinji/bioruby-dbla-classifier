c44f51f9b3496c3cd3730bbba4c838ab6aed0c6f
georgeG <georgkam@gmail.com>
2013-02-22 11:39:49 +0300

    clean

--- lib/bio/sequence/na/na.rb
+++ lib/bio/sequence/na/na.rb
@@ -1,17 +1,13 @@
 class Bio::Sequence::NA
   attr_accessor :mut_pos
 
-  #def mut_pos
-  
-  #end
-
   #position specific polymorphic block 1
-  def pspb1_as_dna(anchor_pos=0,win_len=42)
+  def pspb1_dna(anchor_pos=0,win_len=42)
     self[42 + anchor_pos,win_len]
   end
 
   #position specific polymorphic block 2
-  def pspb2_as_dna(anchor_pos=0,win_len=42)
+  def pspb2_dna(anchor_pos=0,win_len=42)
     if !ww_missing?
       return self[(ww_pos * 3) - 12 - anchor_pos - win_len, win_len]
     elsif !vw_missing?
@@ -22,7 +18,7 @@ class Bio::Sequence::NA
   end
 
   #position specific polymorphic block 3
-  def pspb3_as_dna(anchor_pos=0,win_len=42)
+  def pspb3_dna(anchor_pos=0,win_len=42)
     if !ww_missing?
       return self[(ww_pos * 3) + 42 + anchor_pos, win_len]
     elsif !vw_missing?
@@ -33,7 +29,7 @@ class Bio::Sequence::NA
   end
 
   #position specific polymorphic block 4
-  def pspb4_as_dna(anchor_pos=0,win_len=42)
+  def pspb4_dna(anchor_pos=0,win_len=42)
     self[self.length - 36 - win_len - anchor_pos, win_len]
   end
 
@@ -42,20 +38,20 @@ class Bio::Sequence::NA
   end
 
   def polv2_dna_pos
-   mut_pos.map{|mut| mut - (aa_seq.polv2_pos * 3)}
+    mut_pos.map{|mut| mut - (aa_seq.polv2_pos * 3)}
   end
 
   def polv3_dna_pos
-   mut_pos.map{|mut| mut - (aa_seq.polv3_pos * 3)}
+    mut_pos.map{|mut| mut - (aa_seq.polv3_pos * 3)}
   end
 
   def polv4_dna_pos
-   mut_pos.map{|mut| mut - (aa_seq.polv4_pos * 3)}
+    mut_pos.map{|mut| mut - (aa_seq.polv4_pos * 3)}
   end
 
-
-  def coordinates
-  [polv1_dna_pos, polv2_dna_pos, polv3_dna_pos, polv4_dna_pos]
+  #return an array of distances from each polv
+  def dist_from_polvs
+    [polv1_dna_pos, polv2_dna_pos, polv3_dna_pos, polv4_dna_pos]
   end
 
   private
@@ -76,17 +72,14 @@ class Bio::Sequence::NA
   end
 
   def ww_missing?
-    aa_seq.ww_missing?
-    #true unless aa_seq =~ /WW/i
+    aa_seq.ww_missing? #true unless aa_seq =~ /WW/i
   end
 
   def vw_missing?
-    aa_seq.vw_missing?
-    #true unless aa_seq =~ /VW/i
+    aa_seq.vw_missing? #true unless aa_seq =~ /VW/i
   end
 
   def vw_ww_missing?
-    aa_seq.vw_ww_missing?
-    #true if ww_missing? && vw_missing?
+    aa_seq.vw_ww_missing? #true if ww_missing? && vw_missing?
   end
 end
